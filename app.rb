31fdbe719d1a68c29919b3f119d0601cc237b8d3
LauraMaddaluno <lauralondon.4@gmail.com>
2021-09-07 19:21:03 +0100

    Added a new file for the form and used a POST request and write a root that handles a POST request

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@ get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
-get '/named-cat' do
+
+get '/cat-form' do
+  p params 
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
-
 end
